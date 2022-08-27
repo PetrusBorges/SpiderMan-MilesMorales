d787b6a30b2f1691f9cbd7e0ec76c7f283e4f129
Petrus Borges Machado <contatopetrusborges@gmail.com>
2022-08-27 20:48:10 -0300

    Inclusão de arquivos REACT

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import LoaderElements from './components/Loader'
+import Header from './components/Header'
+import Container from './components/Container'
+import VideoSection from './components/Video'
+
+import './styles/style.scss'
+
+export default function App() {
+  return (
+    <>
+    <LoaderElements/>
+    <Header/>
+    <VideoSection/>
+    <Container/>
+    </>
+  )
+}
